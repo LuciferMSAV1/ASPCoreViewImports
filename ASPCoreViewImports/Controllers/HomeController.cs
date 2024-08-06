abf57c34e928defc698ffb110343cf98c4702075
gangs <gangs@DESKTOP-FETTB1F>
2024-08-06 16:45:45 +0500

    Add project files.

---
+++ ASPCoreViewImports/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using ASPCoreViewImports.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPCoreViewImports.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var students = new List<Student>
+            {
+                new Student {Id = 1,Name = "Kumar",Gender="Male" },
+                new Student {Id = 2, Name = "Adil",Gender="Male"},
+                new Student {Id = 3, Name = "Natasha",Gender="Female" }
+            };
+
+            return View(students);
+        }
+        public IActionResult About()
+        {
+            var students = new List<Student>
+            {
+                new Student {Id = 1,Name = "Kumar",Gender="Male" },
+                new Student {Id = 2, Name = "Adil",Gender="Male"},
+                new Student {Id = 3, Name = "Natasha",Gender="Female" }
+            };
+
+            return View(students);
+        }
+        public IActionResult Contact()
+        {
+            var students = new List<Student>
+            {
+                new Student {Id = 1,Name = "Kumar",Gender="Male" },
+                new Student {Id = 2, Name = "Adil",Gender="Male"},
+                new Student {Id = 3, Name = "Natasha",Gender="Female" }
+            };
+
+            return View(students);
+        }
+    }
+}
